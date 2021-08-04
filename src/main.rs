c08569d89a85f1af4a23c21948a20c60ae5d901b
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-08-04 19:47:19 -0400

    Move parsing code to parse.rs

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,9 @@
+mod parse;
 mod token;
 mod types;
 
-use crate::token::pop_token;
+pub use crate::parse::parse;
 pub use crate::types::*;
-
-#[derive(Debug, Eq, PartialEq)]
-enum ParseError {
-    UnexpectedEOF,
-    UnmatchedClosingParen,
-}
-type ParseResult = Result<Expression, ParseError>;
-
-fn parse_expression(string: &str) -> (ParseResult, String) {
-    match pop_token(string) {
-        (None, remainder) => (Err(ParseError::UnexpectedEOF), remainder),
-        (Some(token), remainder) => match &token as &str {
-            "(" => parse_list(&remainder),
-            ")" => (Err(ParseError::UnmatchedClosingParen), remainder),
-            "'" => match parse_expression(&remainder) {
-                (Err(err), remainder) => (Err(err), remainder),
-                (Ok(quoted_value), remainder) => {
-                    (Ok(list!(atom!("quote"), quoted_value)), remainder)
-                }
-            },
-            token => (Ok(atom!(token)), remainder),
-        },
-    }
-}
-
-/// Parses a list, starting from the first element
-fn parse_list(string: &str) -> (ParseResult, String) {
-    match parse_expression(string) {
-        // An UnmatchedClosingParen actually means we encountered the end of the list
-        // Lists are terminated with a nil atom, so just return that
-        (Err(ParseError::UnmatchedClosingParen), remainder) => (Ok(atom!("nil")), remainder),
-        (Ok(car), remainder) => {
-            let (cdr, remainder) = parse_list(&remainder);
-            if let Ok(cdr) = cdr {
-                (Ok(cons!(&car, &cdr)), remainder)
-            } else {
-                // This will only happen if cdr is an Err
-                (cdr, remainder)
-            }
-        }
-        (Err(err), remainder) => (Err(err), remainder),
-    }
-}
-
-fn parse(string: &str) -> ParseResult {
-    let (result, _) = parse_expression(string);
-    result
-}
-
-#[test]
-fn test_parse() {
-    assert_eq!(parse("aaa"), Ok(atom!("aaa")));
-    assert_eq!(parse("()"), Ok(atom!("nil")));
-    assert_eq!(parse("(aaa)"), Ok(list!(atom!("aaa"))));
-    assert_eq!(
-        parse("  (  aaa   bbb  )  "),
-        Ok(list!(atom!("aaa"), atom!("bbb")))
-    );
-    assert_eq!(parse("("), Err(ParseError::UnexpectedEOF));
-    assert_eq!(parse(")"), Err(ParseError::UnmatchedClosingParen));
-    assert_eq!(parse("'aaa"), Ok(list!(atom!("quote"), atom!("aaa"))));
-    assert_eq!(
-        parse("'(aaa)"),
-        Ok(list!(atom!("quote"), list!(atom!("aaa"))))
-    );
-}
 
 #[derive(Debug, Eq, PartialEq)]
 enum EvaluationError {
@@ -254,13 +189,4 @@ fn test_cdr() {
     );
 }
 
-fn main() {
-    let e: Expression = atom!("yes");
-    println!("Hello, world! {:?}", e);
-    println!("Hello, world! {:?}", evaluate(&e));
-    let e: Expression = cons!(&atom!("A"), &atom!("B"));
-    println!("Hello, world! {:?}", e);
-    println!("Hello, world! {:?}", evaluate(&e));
-    println!("{:?}", pop_token("aa"));
-    println!("{:?}", pop_token("(("));
-}
+fn main() {}
---
+++ src/parse.rs
@@ -0,0 +1,71 @@
+use crate::token::pop_token;
+use crate::*;
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum ParseError {
+  UnexpectedEOF,
+  UnmatchedClosingParen,
+}
+pub type ParseResult = Result<Expression, ParseError>;
+
+fn parse_expression(string: &str) -> (ParseResult, String) {
+  match pop_token(string) {
+    (None, remainder) => (Err(ParseError::UnexpectedEOF), remainder),
+    (Some(token), remainder) => match &token as &str {
+      "(" => parse_list(&remainder),
+      ")" => (Err(ParseError::UnmatchedClosingParen), remainder),
+      "'" => match parse_expression(&remainder) {
+        (Err(err), remainder) => (Err(err), remainder),
+        (Ok(quoted_value), remainder) => (Ok(list!(atom!("quote"), quoted_value)), remainder),
+      },
+      token => (Ok(atom!(token)), remainder),
+    },
+  }
+}
+
+/// Parses a list, starting from the first element
+fn parse_list(string: &str) -> (ParseResult, String) {
+  match parse_expression(string) {
+    // An UnmatchedClosingParen actually means we encountered the end of the list
+    // Lists are terminated with a nil atom, so just return that
+    (Err(ParseError::UnmatchedClosingParen), remainder) => (Ok(atom!("nil")), remainder),
+    (Ok(car), remainder) => {
+      let (cdr, remainder) = parse_list(&remainder);
+      if let Ok(cdr) = cdr {
+        (Ok(cons!(&car, &cdr)), remainder)
+      } else {
+        // This will only happen if cdr is an Err
+        (cdr, remainder)
+      }
+    }
+    (Err(err), remainder) => (Err(err), remainder),
+  }
+}
+
+pub fn parse(string: &str) -> ParseResult {
+  let (result, _) = parse_expression(string);
+  result
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test_parse() {
+    assert_eq!(parse("aaa"), Ok(atom!("aaa")));
+    assert_eq!(parse("()"), Ok(atom!("nil")));
+    assert_eq!(parse("(aaa)"), Ok(list!(atom!("aaa"))));
+    assert_eq!(
+      parse("  (  aaa   bbb  )  "),
+      Ok(list!(atom!("aaa"), atom!("bbb")))
+    );
+    assert_eq!(parse("("), Err(ParseError::UnexpectedEOF));
+    assert_eq!(parse(")"), Err(ParseError::UnmatchedClosingParen));
+    assert_eq!(parse("'aaa"), Ok(list!(atom!("quote"), atom!("aaa"))));
+    assert_eq!(
+      parse("'(aaa)"),
+      Ok(list!(atom!("quote"), list!(atom!("aaa"))))
+    );
+  }
+}
